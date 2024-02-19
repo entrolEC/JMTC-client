0bd688db2ee541adedb1fd39ad2b72a4127301cd
entrolEC <euichan1108@gmail.com>
2024-02-20 02:28:10 +0900

    Feat: language change

--- app/dashboard/ctnrs/page.tsx
+++ app/dashboard/ctnrs/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({
                 <h1 className={`${lusitana.className} text-2xl`}>Ctnrs</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search ctnrs..." />
+                <Search placeholder="CTNR 검색..." />
                 <CreateCtnr />
             </div>
             <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
--- app/dashboard/ports/page.tsx
+++ app/dashboard/ports/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({
                 <h1 className={`${lusitana.className} text-2xl`}>Ports</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search ports..." />
+                <Search placeholder="Port 검색..." />
                 <CreatePort />
             </div>
             <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
