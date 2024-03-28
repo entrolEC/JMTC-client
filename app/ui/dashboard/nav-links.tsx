729f10a035be7c3c9a3ce3843db8acb72373ccad
entrolEC <euichan1108@gmail.com>
2024-03-29 02:29:19 +0900

    Fix: prefetch bug fix

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -2,12 +2,9 @@
 
 import { ClipboardDocumentListIcon, CurrencyDollarIcon, DocumentIcon, MapPinIcon, RectangleStackIcon, TruckIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import clsx from "clsx";
-import { useEffect } from "react"; // Map of links to display in the side navigation.
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
     {
         name: "Quotations",
@@ -43,13 +40,6 @@ const links = [
 
 export default function NavLinks() {
     const pathname = usePathname();
-    const router = useRouter();
-
-    useEffect(() => {
-        links.forEach((link) => {
-            router.prefetch(link.href);
-        });
-    }, [router]);
 
     return (
         <>
@@ -59,6 +49,7 @@ export default function NavLinks() {
                     <Link
                         key={link.name}
                         href={link.href}
+                        prefetch={true}
                         className={clsx(
                             "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-gray-200 hover:text-gray-600 md:flex-none md:justify-start md:p-2 md:px-3",
                             {
